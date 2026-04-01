6926bb548f74555a74be6d47724c79d93a71e40f
KRISH <krishraj25@navgurukul.org>
2026-04-01 09:58:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,69 @@
+// alert("hello bedmosh");
+// // document.getElementById("h").innerHTML=("krish");
+// document.write("hello")
+
+
+
+// console.log("hello krish");
+
+
+
+// let x;
+// x = 5;
+// alert(x);
+
+
+
+
+// let krish = 44;
+// console.log(krish);
+
+// let xx=hello;
+// let yy= "krish";
+// alert(yy);
+
+// let k=10;
+// let r=20;
+// let i=k-r;
+// alert(i)
+
+// let kk=10;
+// kk=kk**4;
+// alert(kk);
+
+
+
+// let aa = 10;
+// let bb = 5;
+// let cc= aa*bb;
+// console.log(cc);
+
+// let hh=10;
+// hh++;
+// console.log(hh);
+// document.getElementById('h').innerHTML=("krish")
+
+
+
+let krish= "hello";
+console.log(krish); 
+
+let ankush=20>40;
+console.log(ankush);
+
+
+
+let anurag=typeof 'hello';
+console.log(anurag);
+
+
+
+let govind = "krish";
+alert(`hello ${govind} `);
+
+
+
+
+
+
+
